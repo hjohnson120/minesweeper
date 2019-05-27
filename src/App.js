6c679aaa580ffb5fea11125c718d960fe19f6f39
Heather Johnson-Ledlie <heatherjohnson120@yahoo.com>
2019-05-27 19:35:32 -0400

    flaggedcell

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+const gameUrl = 'https://minesweeper-api.herokuapp.com/'
+
 class App extends Component {
   state = {
     game: { board: [] }
@@ -24,28 +26,78 @@ class App extends Component {
       })
   }
 
+  cellClick = (row, col) => {
+    console.log('clicked', row, col)
+    fetch(`${gameUrl}games/${this.state.game.id}/check`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({ row: row, col: col })
+    })
+      .then(resp => {
+        return resp.json()
+      })
+      .then(newPlaying => {
+        this.setState({
+          game: newPlaying
+        })
+        console.log({ newPlaying })
+      })
+  }
+
+  flaggedCell = (event, row, col) => {
+    console.log('clicked', row, col)
+    event.preventDefault()
+    fetch(`${gameUrl}games/${this.state.game.id}/flag`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({ row: row, col: col })
+    })
+      .then(resp => {
+        return resp.json()
+      })
+      .then(flagPicked => {
+        this.setState({
+          game: flagPicked
+        })
+        console.log({ flagPicked })
+      })
+  }
+
   render() {
     return (
       <>
-        <div>
-          <table>
-            <tbody>
-              {this.state.game.board.map((row, i) => {
-                return (
-                  <tr key={i}>
-                    {row.map((col, j) => {
-                      return (
-                        <td key={j} className="data">
-                          {this.state.game.board[i][j]}
-                        </td>
-                      )
-                    })}
-                  </tr>
-                )
-              })}
-            </tbody>
-          </table>
-        </div>
+        <main>
+          <body className="table">
+            <table>
+              <tbody>
+                {this.state.game.board.map((row, i) => {
+                  return (
+                    <tr key={i}>
+                      {row.map((col, j) => {
+                        return (
+                          <td
+                            key={j}
+                            className="data"
+                            onClick={() => this.cellClick(i, j)}
+                            onContextMenu={event =>
+                              this.flaggedCell(event, i, j)
+                            }
+                          >
+                            {this.state.game.board[i][j]}
+                          </td>
+                        )
+                      })}
+                    </tr>
+                  )
+                })}
+              </tbody>
+            </table>
+          </body>
+        </main>
       </>
     )
   }
